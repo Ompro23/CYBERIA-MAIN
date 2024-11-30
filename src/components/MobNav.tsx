ec1fb64b21f3b3eea5fe11fcd07aa33f437298c8
Aditya-Garg101 <gargaditya880@gmail.com>
2024-11-30 16:40:50 +0530

    COmits

--- src/components/MobNav.tsx
+++ src/components/MobNav.tsx
@@ -23,14 +23,14 @@ const MobNav = () => {
         closeMenu()
     };
     return (
-        <div className={`border ${isOpen? "h-full " : "h-[8vh]"}  backdrop-blur-lg bg-transparent z-50 border-white/[0.1] my-2 mx-2 rounded-xl  fixed  top-0 left-0 right-0  transition-all duration-400 ease-in-out  block `}>
+        <div className={`border ${isOpen? "h-full " : "h-[65px]"}  backdrop-blur-lg bg-transparent z-50 border-white/[0.1] my-2 mx-2 rounded-xl  fixed  top-0 left-0 right-0  transition-all duration-400 ease-in-out  block `}>
             <div className="flex  h-[86px] w-full">
-                <div className="flex ms-3 h-full gap-5 p-2 w-3/4">
-                    <img src="/Logo/msu.png" className="object-contain" alt="" />
-                    <img src="/Logo/75.png" className="object-contain" alt="" />
+                <div className="flex ms-3 h-[70px] gap-5 p-2 w-3/4">
+                    <img src="/Logo/msu.png" className="object-contain w-[50px]" alt="" />
+                    <img src="/Logo/75.png" className="object-contain w-[50px]" alt="" />
                     
                 </div>
-                <div className="flex h-full  justify-center items-center  p-1 w-1/4">
+                <div className="flex h-[70px]  justify-center items-center  p-1 w-1/4">
                 {isOpen ? <IoClose className="w-6 h-6" onClick={toggleMenu} /> : <FaBarsStaggered onClick={toggleMenu} className="text-white/[0.6]" /> }                  
                 </div>
             </div>
