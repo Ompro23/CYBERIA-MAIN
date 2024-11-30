ad72c9fe38718470f2587e28a19fa7119b136ee9
Aditya-Garg101 <gargaditya880@gmail.com>
2024-11-30 16:54:03 +0530

    COmijcn

--- src/app/(pages)/selectedEvent/page.tsx
+++ src/app/(pages)/selectedEvent/page.tsx
@@ -12,6 +12,7 @@ import MobileNav from "@/components/MobileNav";
 import MobNav from "@/components/MobNav";
 import { SignupFormTeam } from "@/components/RegistrationFormTeam";
 import { ShimmerButtonDemo } from "@/components/ShrimmerButton";
+import { AnimatedGradientTextDemo } from "@/components/RegisterButton";
 // ... other imports
 
 const selectedEvent = () => {
@@ -111,11 +112,16 @@ const selectedEvent = () => {
                     </div>
 
                     <div className="flex my-5 justify-center items-center w-full ">
-                    <TabsList className="bg-transparent hover:bg-white rounded-lg">
+                      <TabsList className="bg-transparent ">
+                      <TabsTrigger value="register">
+                      <AnimatedGradientTextDemo/>
+                      </TabsTrigger>
+                      </TabsList>
+                    {/* <TabsList className="bg-transparent hover:bg-white rounded-lg">
                       <TabsTrigger value="register"  className="flex overflow-hidden items-center text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 bg-black text-white shadow hover:bg-black/90 h-9 px-4 py-2 max-w-52 whitespace-pre md:flex group relative w-full justify-center gap-2 rounded-md transition-all duration-300 ease-out hover:ring-2 hover:ring-black hover:ring-offset-2">
                         <span className="absolute right-0 -mt-12 h-32 w-8 translate-x-12 rotate-12  opacity-10 transition-all duration-1000 ease-out group-hover:-translate-x-40"></span>
                         <div className="flex items-center">
-                          <span className="ml-1 text-white">Register Now</span>
+                          <AnimatedGradientTextDemo/>
                         </div>
                         <div className="ml-2 flex items-center gap-1 text-sm md:flex">
                           <svg
@@ -134,7 +140,7 @@ const selectedEvent = () => {
                           </svg>
                         </div>
                       </TabsTrigger>
-                      </TabsList>
+                      </TabsList> */}
                     </div>
                   </div>
                 ) : (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -98,7 +98,8 @@ const config: Config = {
   			marquee: 'marquee var(--duration) infinite linear',
   			'marquee-vertical': 'marquee-vertical var(--duration) linear infinite',
   			'shimmer-slide': 'shimmer-slide var(--speed) ease-in-out infinite alternate',
-  			'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear'
+  			'spin-around': 'spin-around calc(var(--speed) * 2) infinite linear',
+  			gradient: 'gradient 8s linear infinite'
   		},
   		keyframes: {
   			move: {
@@ -179,6 +180,11 @@ const config: Config = {
   				},
   				'100%': {
   					transform: 'translateZ(0) rotate(360deg)'
+  				}
+  			},
+  			gradient: {
+  				to: {
+  					backgroundPosition: 'var(--bg-size) 0'
   				}
   			}
   		}
