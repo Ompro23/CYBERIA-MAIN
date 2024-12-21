e69065ff231c28699441c990bd8434c799eb6a54
Om vataliya <109670967+Omcodes23@users.noreply.github.com>
2024-12-22 02:14:42 +0530

    add the dates extention alert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
-import type { Metadata } from "next";
+"use client";
+import React, { useEffect, useState } from "react";
 import localFont from "next/font/local";
 import "./globals.css";
-import { NavbarDemo } from "@/components/Navbar";
 import EventsContextProvider from "./context/MyContext";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 import Footer from "@/components/Footer";
-import MobileNav from "@/components/MobileNav";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -18,27 +17,48 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "Cyberia",
-  description: "Unleash the geek within you",
-  
-};
+const CustomAlert = ({ message, onClose }: { message: string; onClose: () => void }) => (
+  <div className="fixed inset-0 flex items-center justify-center z-50 p-4 bg-black/50">
+    <div className="bg-black p-6 rounded-lg shadow-lg text-center animate-fade-in bg-opacity-90 border border-gray-800  sm:max-w-md md:max-w-lg lg:max-w-sm xl:max-w-sm">
+      <p className="text-white text-base md:text-lg lg:text-xl">{message}</p>
+      <button
+        onClick={onClose}
+        className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-md sm:w-auto hover:bg-blue-600 transition"
+      >
+        Roger that
+      </button>
+    </div>
+  </div>
+);
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setShowAlert(true);
+    }
+  }, []);
+
   return (
-    <html lang="en"  className="dark h-full w-full">      
+    <html lang="en" className="dark h-full w-full">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full`}
-      >       
-       <EventsContextProvider>
-      
-        {children}        
-        <Toaster/>        
-        <Footer/>        
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full bg-gradient animate-gradient`}
+      >
+        <EventsContextProvider>
+          {children}
+          <Toaster />
+          <Footer />
+          {showAlert && (
+            <CustomAlert
+              message="Cyberia's dates have been extended and new dates will be released very soon"
+              onClose={() => setShowAlert(false)}
+            />
+          )}
         </EventsContextProvider>
       </body>
     </html>
