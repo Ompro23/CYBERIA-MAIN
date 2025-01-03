1eee3a8294f84bd3a2d0ddebb5fce1b045293957
Aditya-Garg101 <gargaditya880@gmail.com>
2025-01-03 23:55:37 +0530

    trial

--- src/app/api/payments/route.js
+++ src/app/api/payments/route.js
@@ -45,7 +45,7 @@ export async function POST(request) {
   try {
     const body = await request.json()    
     const requestData = {
-      "order_amount": body.price,
+      "order_amount": 1,
       "order_currency": "INR",
       "order_id": createOrderId(),
       "customer_details": {
