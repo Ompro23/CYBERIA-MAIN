66d7a4e3fd62e9087df8e1262c4cab1ee1a2500d
Aditya-Garg101 <gargaditya880@gmail.com>
2025-01-04 23:37:30 +0530

    Cashfree bot payment integration

---
+++ src/app/(pages)/chatbot/page.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import Image from "next/image";
+import { message } from "antd";
+import { useContext, useEffect, useState } from "react";
+const Cashfree = require("@cashfreepayments/cashfree-js");
+import axios from "axios";
+import Button from "./waButton";
+import { eventContext } from "@/app/context/MyContext";
+import { HOST } from "@/lib/utilities";
+
+interface FormData {
+  fullName: string;
+  email: string;
+  contactNo: string;
+  contactNo2: string;
+  institute: string;
+  year: string;
+  level: string;
+  age: string;
+  gender: string;
+  stream: string;
+  events: string[];
+  members: number,
+  member: string[],
+  teamLeader: string,
+  teamName: string;
+}
+
+export default function page() {
+  const [Data, setData] = useState<FormData>({
+    fullName: "",
+    email: "",
+    teamName : "",
+    teamLeader:"",
+    contactNo: "",
+    contactNo2: "",
+    institute: "",
+    year: "",
+    level: "",
+    age: "",
+    gender: "",
+    stream: "",
+    member: [],
+    members: 0,
+    events: [],
+  });
+
+  const eventContextValue = useContext(eventContext);
+  const ticketData = eventContextValue?.ticketData;
+      const setTicketData = eventContextValue?.setTicketData;
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const searchParams = new URLSearchParams(window.location.search);
+      
+      const data: FormData = {
+        fullName: searchParams.get("fullName") || "",
+        email: searchParams.get("email") || "",
+        contactNo: searchParams.get("contactNo") || "",
+        contactNo2: searchParams.get("contactNo2") || "",
+        institute: searchParams.get("institute") || "",
+        year: searchParams.get("year") || "",
+        level: searchParams.get("level") || "",
+        age: searchParams.get("age") || "",
+        gender: searchParams.get("gender") || "",
+        stream: searchParams.get("stream") || "",
+        members: Number(searchParams.get("members")) || 0,  
+        teamLeader: searchParams.get("teamLeader") || "",  
+        teamName: searchParams.get("teamName") || "",  
+        events: searchParams.get("events")?.split(",") || [], // Ensure array fallback
+        member: searchParams.get("member")?.split(",") || [], // Ensure array fallback
+      };
+
+      setData(data); // Set data to state
+    } else {
+      console.error("Window is not defined");
+    }
+  }, []);
+
+  const [paymentDone, setpaymentDone] = useState(false);
+
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    message.info("Please wait...");
+    try {
+      const cashfree = await Cashfree.load({
+        mode: "production", // Change to 'test' for testing
+      });
+
+      console.log(Data);
+
+      
+      const response = await axios.post(
+        `${HOST}/api/botPayments`,
+        { ...Data },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(response)
+      if (!response.data) {
+        message.info("Please try again");
+        return;
+      }
+
+      const checkoutOptions = {
+        paymentSessionId: response.data.data.payment_session_id,
+        redirectTarget: "_modal",
+        appearance: {
+          width: "700px",
+          height: "700px",
+        },
+      };
+
+      const result = await cashfree.checkout(checkoutOptions);
+
+      console.log(result)
+      if (result.redirect) {
+        message.success("Payment Completed");
+      } else if (result.error) {
+        message.error("Payment failed. Please try again.");
+        console.error(result.error);
+      } else if (result.paymentDetails) {
+        message.success("Payment has been completed");
+        message.info("Please wait for redirection");
+
+        const apiEndpoint = Data.teamName 
+        ? `https://cyberia-node-server.vercel.app/api/user/registerTeamUser` 
+        : `https://cyberia-node-server.vercel.app/api/user/registerSoloUser`;
+
+        const registerResponse = await axios.post(
+          apiEndpoint,
+          { ...Data },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        console.log(registerResponse.data)
+
+        if (registerResponse.status === 201 && registerResponse.data.SoloData) {
+          message.success("Registration successful");
+          setpaymentDone(true);
+          setTicketData?.(registerResponse.data.SoloData)
+        } else {
+          message.error("An error occurred. Contact customer support.");
+        }
+      }
+    } catch (error) {
+      message.error("Technical error or invalid credentials");
+      console.error(error);
+    }
+  };
+
+  const handleWhatsapp = async (e: React.MouseEvent<HTMLButtonElement>) =>{
+    const phoneNumber = "+1 (415) 523-8886";
+    const cleanPhoneNumber = phoneNumber.replace(/[^0-9]/g, ""); // Remove non-numeric characters
+    
+    window.location.href = `https://wa.me/${cleanPhoneNumber}?text=${encodeURIComponent(ticketData?.ticket)}`;
+    
+  }
+
+  return (
+    <div className="flex w-full justify-center flex-col gap-5 items-center h-screen text-center">
+      <h1 className="text-3xl font-bold">Welcome! to Cyberia Payment Page</h1>
+      <button
+        onClick={handleSubmit}
+        className="text-white font-semibold px-5 py-1 text-xl border-2 rounded-md"
+        type="button"
+      >
+        Pay Now
+      </button>
+      {paymentDone &&
+      <Button onClick={handleWhatsapp} />}
+
+    </div>
+  );
+}
---
+++ src/app/api/botPayments/route.js
@@ -0,0 +1,100 @@
+import { NextResponse } from 'next/server';
+import { connectDb } from '@/helper/db';
+import { Events } from '@/models/Events';
+import dotenv from 'dotenv';
+import { Cashfree } from 'cashfree-pg';
+import crypto from 'crypto';
+import { EventEmitter } from 'stream';
+
+dotenv.config();
+// Connect to the database
+connectDb()
+// CORS headers
+const setCorsHeaders = (response) => {
+  // Allow any origin (be sure to restrict this to specific origins in production)
+  response.headers.set('Access-Control-Allow-Origin', 'https://msudcacyberia.in');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS'); // Allow all necessary methods
+  response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept'); // Allow necessary headers
+  response.headers.set('Access-Control-Allow-Credentials', 'true'); // If you want to send credentials (cookies, HTTP authentication, etc.)
+
+  return response;
+};
+
+Cashfree.XClientId = process.env.CLIENT_ID;
+Cashfree.XClientSecret = process.env.CLIENT_SECRET;
+Cashfree.XEnvironment = Cashfree.Environment.PRODUCTION;
+
+// Function to create order ID
+function createOrderId() {
+  const uniqueId = crypto.randomBytes(16).toString("hex");
+
+  const hash = crypto.createHash("sha256");
+  hash.update(uniqueId);
+
+  const orderId = hash.digest("hex");
+
+  return orderId.substr(0, 12);
+}
+
+// POST request handler
+export async function POST(request) {
+  try {
+    const body = await request.json();
+
+    console.log(body)
+    // Fetch the event data from the database based on the event ID
+    const eventId = body.events;
+    console.log(eventId)
+     // Assuming the event ID is passed in the request
+     const event = await Events.find({ title: { $in: body.events } });
+
+    
+    if (!event) {
+      return NextResponse.json({ error: "Event not found" }, { status: 404 });
+    }
+
+    // Calculate the price based on event data
+    const totalPrice = event.reduce((sum, event) => sum + (event.price || 0), 0);
+    
+
+   
+    console.log(totalPrice);
+
+    // Use the event price in the order request
+    const requestData = {
+      "order_amount": totalPrice, // Use the calculated price
+      "order_currency": "INR",
+      "order_id": createOrderId(),
+      "customer_details": {
+        "customer_id": "node_sdk_prod",
+        "customer_name": body.fullName,
+        "customer_email": body.email,
+        "customer_phone": body.contactNo,
+      },
+      "order_meta": {
+        "return_url": `https://merchant.in/pg/process_return?cf_id=${createOrderId()}`,
+      },
+    };
+
+    // Format the date
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString().split('T')[0];
+
+    // Await the Cashfree.PGCreateOrder call
+    const response = await Cashfree.PGCreateOrder("2023-08-01", requestData);
+
+    // Extract the data from the response
+    const a = response.data;
+
+    // Return the success response
+    const apiResponse = NextResponse.json({
+      success: true,
+      data: a, // Directly using the response data
+    }, { status: 200 });
+
+    return setCorsHeaders(apiResponse);
+  } catch (error) {
+    console.log(error.message);
+    return NextResponse.json({ error }, { status: 400 });
+  }
+}
